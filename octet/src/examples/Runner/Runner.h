c87e7297f34c7fcdceb876713df11f85f02fe0b7
Jeyp <jp.evette1@gmail.com>
2015-12-04 12:48:22 +0000

    added path + implemented camera following mouse for debug

--- octet/src/examples/Runner/Runner.h
+++ octet/src/examples/Runner/Runner.h
@@ -9,39 +9,86 @@ namespace octet {
   class Runner : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+	bool freeCamera;
+
+	vec3 cameraPos;
+	quat cameraRot;
+
   public:
     /// this is called when we construct the class before everything is initialised.
     Runner(int argc, char **argv) : app(argc, argv) {
     }
-
+	// x = right
+	// y = Up
+	// z = straight
     /// this is called once OpenGL is initialized
     void app_init() {
+	  freeCamera = true;
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
+	  cameraPos = app_scene->get_camera_instance(0)->get_node()->get_position();
+	  cameraRot = app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().toQuaternion();
+	  app_scene->get_camera_instance(0)->set_far_plane(1000.0f);
+
 
       material *red = new material(vec4(1, 0, 0, 1));
-      mesh_box *box = new mesh_box(vec3(4));
+
+
+      mesh_box *box = new mesh_box(vec3(5,0.1f,100000));
+
+
+
       scene_node *node = new scene_node();
       app_scene->add_child(node);
       app_scene->add_mesh_instance(new mesh_instance(node, box, red));
+	  node->translate(vec3(0,-10,0));
     }
+
+
+	void cameraFollowMouse()
+	{
+		float sensitivity = 100.0f / 360.0f;
+		int xPos, yPos;
+		get_absolute_mouse_movement(xPos, yPos);
+		set_mouse_pos(0, 0);
+
+		mat4t &camera_to_world = app_scene->get_camera_instance(0)->get_node()->access_nodeToParent();
+
+		camera_to_world.x() = vec4(1, 0, 0, 0);
+		camera_to_world.y() = vec4(0, 1, 0, 0);
+		camera_to_world.z() = vec4(0, 0, 1, 0);
+
+		float angle_x = (float)(app_scene->get_camera_instance(0)->get_node()->get_position().x()-xPos) * sensitivity;
+		float angle_y = (float)(app_scene->get_camera_instance(0)->get_node()->get_position().y() -yPos) * sensitivity;
+		angle_y = std::max(-90.0f, std::min(angle_y, 90.0f));
+		camera_to_world.rotateY(angle_x);
+		camera_to_world.rotateX(angle_y);
+	}
+
+	void updateCamera()
+	{
+		if (freeCamera)
+		{
+			cameraFollowMouse();
+		}
+	}
+
 
     /// this is called to draw the world
     void draw_world(int x, int y, int w, int h) {
       int vx = 0, vy = 0;
       get_viewport_size(vx, vy);
       app_scene->begin_render(vx, vy);
-
+	  
       // update matrices. assume 30 fps.
       app_scene->update(1.0f/30);
+	  updateCamera();
 
       // draw the scene
       app_scene->render((float)vx / vy);
 
       // tumble the box  (there is only one mesh instance)
       scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 0, 0));
-      node->rotate(1, vec3(0, 1, 0));
     }
   };
 }
--- octet/src/math/quat.h
+++ octet/src/math/quat.h
@@ -11,13 +11,39 @@ namespace octet { namespace math {
   class quat : public vec4
   {
   public:
-    quat(float x, float y, float z, float w) : vec4(x, y, z, w) {}
+	quat() {}
+	quat(float x, float y, float z, float w) : vec4(x, y, z, w) {}
     quat(const vec4 &r) { *(vec4*)this = r; }
     quat operator*(const quat &r) const { return quat(qmul(r)); }
     quat operator*(float r) const { return quat((vec4&)*this * r); }
     quat &operator*=(const quat &r) { *(vec4*)this = qmul(r); return *this; }
     quat conjugate() const { return qconj(); }
     vec4 rotate(const vec4 &r) const { return (*this * r) * conjugate(); }
+	quat fromAxisAngle(vec3 axis, float angle)
+	{
+		float x = axis.x() * sin(angle / 2);
+		float y = axis.y() * sin(angle / 2);
+		float z = axis.z() * sin(angle / 2);
+		float w = cos(angle / 2);
+		*(vec4*)this = vec4(x, y, z, w);
+	}
+	void toAxisAngle(vec3 &axis, float &angle)
+	{
+		float x, y, z;
+		if (this->w() == 1.0f)
+		{
+			x = y = z = 0.0f;
+		}
+		else
+		{
+			x = this->x() / sqrt(1.0f - this->w());
+			y = this->y() / sqrt(1.0f - this->w());
+			z = this->z() / sqrt(1.0f - this->w());
+		}
+		float w = 2 * acos(this->w());
+		axis = vec3(x, y, z);
+		angle = w;
+	}
   };
 
 } }
